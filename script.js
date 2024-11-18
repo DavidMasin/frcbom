9b394874a19138815c6cda07901828e8b2b439ea
David Masin <davidmasin2910@gmail.com>
2024-11-18 20:16:22 +0200

    Debug Client 5

--- script.js
+++ script.js
@@ -183,7 +183,7 @@ document.addEventListener('DOMContentLoaded', () => {
         window.location.href = 'index.html';
     });
     document.getElementById('logoutButton')?.addEventListener('click', handleLogout);
-    document.getElementById('fetchBOMButton')?.addEventListener('click', handleFetchBOM);
+//    document.getElementById('fetchBOMButton')?.addEventListener('click', handleFetchBOM);
 
     // Display team number in header
     if (document.getElementById('teamNumber')) {

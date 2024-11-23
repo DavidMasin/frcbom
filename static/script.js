ebe71bde5c3e47b3c57989eab4f08229aa502e8e
David Masin <davidmasin2910@gmail.com>
2024-11-23 21:31:52 +0200

    Updated to login: api/login

--- static/script.js
+++ static/script.js
@@ -21,7 +21,7 @@ async function handlePasswordSubmit() {
     }
 
     try {
-        const response = await fetch(`${API_BASE_URL}/login`, {
+        const response = await fetch(`${API_BASE_URL}api/login`, {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({team_number: teamNumber, password})

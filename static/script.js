c1fd246a8249a6d70e4e057baca4ce8b0a222db4
David Masin <davidmasin2910@gmail.com>
2024-12-06 21:23:50 +0200

    Debug - disable everyone can access 5987/Admin

--- static/script.js
+++ static/script.js
@@ -928,6 +928,7 @@ async function initializeDashboard() {
     if (admin)
     {
         console.log("IM HEREEEE: ",admin)
+        console.log("Is role not admin: ",localStorage.getItem("role")==="Admin")
         if (! localStorage.getItem("role")==="Admin")
         {
             alert('You must be logged in to an ADMIN account to access this dashboard.');

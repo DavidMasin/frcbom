ae9ebad2c71163608d5ef0e2d14a24a28f176c48
David Masin <davidmasin2910@gmail.com>
2024-12-18 00:54:40 +0200

    Revert Bom material

--- app.py
+++ app.py
@@ -194,8 +194,8 @@ def getPartsDict(bom_dict, partNameID, DescriptionID, quantityID, materialID, ma
         part_process1 = row.get("headerIdToValue", {}).get(process1ID, "Unknown")
         part_process2 = row.get("headerIdToValue", {}).get(process2ID, "Unknown")
         part_id = row.get("itemSource", {}).get("partId", "Unknown")
-        if part_material_bom == "Unknown":
-            part_material_bom = part_material["displayName"]
+        # if part_material_bom == "Unknown" and part_material_bom is not None and part_material is not None:
+        #     part_material_bom = part_material["displayName"]
         if part_material != "N/A" and part_material is not None:
             partDict[part_name] = (part_description,
                                    int(quantity), part_material["displayName"], part_material_bom, part_preProcess,

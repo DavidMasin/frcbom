979a52ae80bc25aacbbb6402c59e072a14aef5ae
David Masin <davidmasin2910@gmail.com>
2025-08-02 15:28:02 +0300

    Debugging

--- app.py
+++ app.py
@@ -893,7 +893,7 @@ def fetch_bom():
         response = client.api_client.request(
             'GET',
             url="https://cad.onshape.com" + bom_url,
-            query_params={"indented": False},
+            query_params={"indented": True},
             headers=headers,
             body={}
         )
